9095750f0f71ec72c94c0f377454f71cba35e32f
(martin selle) (atelier.martin.selle@gmail.com) <atelier.martin.selle@gmail.com>
2024-11-27 18:33:10 +0100

    EXO02-RAILS-YELPMVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+puts "Creating restaurants..."
+Restaurant.create!(name: "Epicure", address: "75008 Paris", category: "french")
+Restaurant.create!(name: "Sushi Samba", address: "London", category: "japanese")
+Restaurant.create!(name: "Pizza East", address: "Shoreditch", category: "italian")
+Restaurant.create!(name: "Chez Léon", address: "Brussels", category: "belgian")
+Restaurant.create!(name: "Great Wall", address: "Chinatown", category: "chinese")
+puts "Finished!"
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
